0f1154d4531f078a92264ae4b4a81e954bf07cb0
fedorovVL <fedorovVL@gmail.com>
2017-06-19 20:00:45 +0300

    v_2.1

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3020, function(){
-	console.log('listening on *:3020');
+http.listen(8080, function(){
+	console.log('listening on *:8080');
 });
